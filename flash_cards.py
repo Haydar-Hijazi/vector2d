9304bfc26ae39ebe9a8c0ff0bff7834a70bb3954
Haydar-Hijazi <121183927+Haydar-Hijazi@users.noreply.github.com>
2023-03-02 16:26:59 +0000

    Flash Cards WIP

--- flash_cards.py
+++ flash_cards.py
@@ -1,3 +1,4 @@
+import random
 from vector2d import Vector2d
 def print_conversion(vector,type):
     print("-" * 30)
@@ -11,3 +12,6 @@ def print_conversion(vector,type):
     print(f"to {type} form")
     print("-" * 30)
 e = Vector2d(3,4)
+def generate_vector():
+   return Vector2d(random.randint(-20,21),random.randint(-20,21))
+def print_conversion_solution():
