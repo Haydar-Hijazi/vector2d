49d9a2eb3f1c65cd4bb20742cc85c20b46a1d08d
Haydar-Hijazi <121183927+Haydar-Hijazi@users.noreply.github.com>
2023-03-09 00:31:11 +0000

    Modifications to file

--- flash_cards.py
+++ flash_cards.py
@@ -35,13 +35,13 @@ def print_rectangular_conversion_solution(vector):
    print("-" * 30)
 #Menu
 def main_menu():
- print("#"*20)
+ print("="*20)
  print("Flash Cards")
  print("1. Conversions")
  print("2. Addition")
  print("q. Quit")
  print("Choose an option!")
- print("#"*20,"\n")
+ print("="*20,"\n")
  mode = input("> ")
  if mode == "q":
   print("\nGoodbye!")
@@ -51,12 +51,13 @@ def main_menu():
  if mode == "1":
   if __name__ == '__main__':
     while True:
-      print("#"*50)
+      print("="*25)
+      print("Choose Conversion:")
       print("p. Polar to Rectangular")
       print("r. Rectangular to Polar")
       print("m. Mixed Practice")
       print("q. Return to Main Menu")
-      print("#"*50)
+      print("="*25)
       practice = input()
       if practice == "q":
        print(main_menu())
@@ -93,6 +94,8 @@ def main_menu():
         print_rectangular_conversion_solution(b)
         print("Press Enter to continue")
         input()
+       
+
  else:
    print("\n!!!Please choose one of the available options!!!\n")
    print(main_menu())
