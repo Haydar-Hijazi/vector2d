fa4769ba2b72d12f6e84128489c9bf480f7d615a
Haydar-Hijazi <121183927+Haydar-Hijazi@users.noreply.github.com>
2023-03-10 17:31:04 +0000

    completed mixed

--- flash_cards.py
+++ flash_cards.py
@@ -2,20 +2,24 @@ import random
 from vector2d import Vector2d
 #Polar Conversion to Rectangular
 def print_polar_conversion(vector):
+    print("="*30)
     print("-" * 30)
     print("Convert")
     print("-"*30)
     print(vector)
     print("to polar form")
     print("-" * 30)
+    print("="*30)
 #Rectangular Conversion to Polar
 def print_rectangular_conversion(vector):
+    print("="*30)
     print("-" * 30)
     print("Convert")
     print(f"{abs(vector)} + {vector.angle()}")
     print("-"*30)
     print("to rectangular form")
     print("-" * 30)
+    print("="*30)
 #Random Vector Generator
 def generate_vector():
    return Vector2d(random.randint(-20,21),random.randint(-20,21))
@@ -33,51 +37,76 @@ def print_rectangular_conversion_solution(vector):
    print("-"*30)
    print(vector)
    print("-" * 30)
+ #Rectangular Flash Card
+def rectangular_flash_card():
+  a = generate_vector()
+  print_rectangular_conversion(a)
+  print("Press Enter to see the answer")
+  input()
+  print_rectangular_conversion_solution(a)
+  print("Press Enter to continue or q to quit")
+  n = input()
+  if n == "":
+   print(rectangular_flash_card())
+  if n == "q":
+    print(conversion_menu())
+#Polar Flash Card
+def polar_flash_card(): 
+  a = generate_vector()
+  print_polar_conversion(a)
+  print("Press Enter to see the answer")
+  input()
+  print_polar_conversion_solution(a)
+  print("Press Enter to continue or q to quit")
+  n = input()
+  if n == "":
+   print(polar_flash_card())
+  if n == "q":
+    print(conversion_menu())
 #Conversion Menu
 def conversion_menu():
    if __name__ == '__main__':
     while True:
       print("="*25)
       print("Choose Conversion:")
+      print("-"*25)
       print("p. Polar to Rectangular")
       print("r. Rectangular to Polar")
       print("m. Mixed Practice")
       print("q. Return to Main Menu")
       print("="*25)
       practice = input()
-      a = generate_vector()
       if practice == "q":
        print(main_menu())
       if practice == "p":
-       print_polar_conversion(a)
-       print("Press Enter to see the answer")
-       input()
-       print_polar_conversion_solution(a)
-       print("Press Enter to continue")
-       input()
+        print(rectangular_flash_card())
       if practice == "r":
-       print_rectangular_conversion(a)
-       print("Press Enter to see the answer")
-       input()
-       print_rectangular_conversion_solution(a)
-       print("Press Enter to continue")
-       input()
+        print(polar_flash_card())
       if practice == "m":
-       mixed = random.randint(1,2)
-       if mixed == 1:
-         print_polar_conversion(a)
-         print("Press Enter to see the answer")
-         input()
-         print_polar_conversion_solution(a)
-         print("Press Enter to continue")
-         input()
-       if mixed == 2:
+       while True:
+        r = random.randint(1,2)
+        a = generate_vector()
+        if r == 1:
          print_rectangular_conversion(a)
          print("Press Enter to see the answer")
          input()
          print_rectangular_conversion_solution(a)
-         print("Press Enter to continue")
-         input()
+         print("Press Enter to continue or q to quit")
+         n = input()
+         if n == "q":
+          print(conversion_menu())
+        if r == 2:
+          print_polar_conversion(a)
+          print("Press Enter to see the answer")
+          input()
+          print_polar_conversion_solution(a)
+          print("Press Enter to continue or q to quit")
+          n = input()
+          if n == "q":
+           print(conversion_menu())
+
+          
+         
 #Addition Menu
 def addition_menu():
  pass
@@ -85,6 +114,7 @@ def addition_menu():
 def main_menu():
  print("="*20)
  print("Flash Cards")
+ print("-"*20)
  print("1. Conversions")
  print("2. Addition")
  print("q. Quit")
